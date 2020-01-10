715b232dfad7cfcce24d0f63ff981bb961f43441
garciajnatalie <37346907+garciajnatalie@users.noreply.github.com>
2020-01-09 19:18:42 -0500

    2, 1 example

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-alert (Hello);
+//var hello = 'hello';
+
+//at the end of a statement put a semicolon
+
+//event of the page being fully loaded
+document.addEventListener('DOMContentLoaded', function() {
+
+   //alert('hello');
+   //2 1 on the page. what this is doing is waiting for the page to be fully loaded until code runs document.addEventListener is important and we'll need to most projects
+   console.log(1);
+
+});
+
+console.log(2);
